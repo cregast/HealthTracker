d6e1fe9d8d3651c3b697b39c5a4cfdcb49517a48
Steve Creger <stevecreger@hotmail.com>
2026-06-06 16:51:39 -0500

    Add graphs page

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE = 'vitals-v1';
+const CACHE = 'vitals-v2';
 const ASSETS = [
   '/',
   '/index.html',
+  '/trends.html',
   '/manifest.json',
   'https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&family=Syne:wght@400;700;800&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js',
 ];
 
 self.addEventListener('install', e => {
